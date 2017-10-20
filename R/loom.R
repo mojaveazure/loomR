60fa681df205f46ff20d4ae5bf4fe8bf1eb348e1
Paul Hoffman <phoffman@nygenome.org>
2017-10-20 18:05:32 -0400

    Initialization function

--- R/loom.R
+++ R/loom.R
@@ -1,12 +1,33 @@
-#' @import methods
 #' @import h5
+#' @importFrom methods setClass setMethod setGeneric callNextMethod
 NULL
 
 #' A class for loom
 #'
+#' @name loom-class
+#' @rdname loom-class
 #' @exportClass loom
 #'
 loom <- setClass(
   Class = 'loom',
+  slots = c(
+    version = 'ANY'
+  ),
   contains = 'H5File'
+)
+
+#' @importFrom utils packageVersion
+#'
+setMethod(
+  f = 'initialize',
+  signature = 'loom',
+  definition = function(.Object, name, mode = 'a') {
+    .Object <- callNextMethod(
+      .Object,
+      name = name,
+      mode = mode
+    )
+    .Object@version <- packageVersion(pkg = 'loom')
+    return(.Object)
+  }
 )
