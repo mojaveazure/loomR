77fa2ee128abd199fe0e604df5eb4f2da6029703
Paul Hoffman <phoffman@nygenome.org>
2018-05-03 18:32:34 -0400

    Attempt to get Travis working

---
+++ travis_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo piefail
+
+if [ "${TRAVIS_OS_NAME}" != 'osx' ]; then
+    cd ..
+    wget "${HDF5_RELEASE_URL}/hdf5-${HDF5_VERSION%.*}/hdf5-${HDF5_VERSION}/src/hdf5-${HDF5_VERSION}.tar.gz"
+    tar -xzf "hdf5-${HDF5_VERSION}.tar.gz"
+    cd "hdf5-${HDF5_VERSION}"
+    ./configure --prefix=/usr/local
+    sudo make install
+    cd ../loomR
+fi
